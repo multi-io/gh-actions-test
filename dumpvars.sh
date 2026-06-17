cbb46852f2bbd45996ff76e94ee51c23d0da225c
Olaf Klischat <olaf.klischat@gmail.com>
2026-06-17 17:27:26 +0200

    dumpvars.sh

--- dumpvars.sh
+++ dumpvars.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+echo "dumpvars $1"
+
 echo "MY_ENV_VAR=${MY_ENV_VAR}"
 echo "MY_ENV_SECRET=${MY_ENV_SECRET}"
+echo "MY_REPO_VAR=${MY_REPO_VAR}"
 echo "MY_REPO_SECRET=${MY_REPO_SECRET}"
-echo "MY_ENV_VAR=${MY_ENV_VAR}"
